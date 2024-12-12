ebaea18e06c9529805b19b35c5ab2be78e8badb9
Matuzay32 <eze.rey92@gmail.com>
2024-12-12 12:57:01 -0300

    adding property DTO

--- src/properties/dto/create-property.dto.ts
+++ src/properties/dto/create-property.dto.ts
@@ -1,1 +1,27 @@
-export class CreatePropertyDto {}
+import {
+  IsEnum,
+  IsNotEmpty,
+  IsNumber,
+  IsPositive,
+  IsString,
+} from 'class-validator';
+import { Sector } from '../enums/sector.enum';
+
+export class CreatePropertyDto {
+  @IsString()
+  @IsNotEmpty({ message: 'Address is required' })
+  address: string;
+
+  @IsNumber()
+  @IsPositive({ message: 'Area must be a positive number' })
+  area: number;
+
+  @IsEnum(Sector, {
+    message: `Sector must be one of the following: ${Object.values(Sector).join(', ')}`,
+  })
+  sector: Sector;
+
+  @IsString()
+  @IsNotEmpty({ message: 'Owner name is required' })
+  owner_name: string;
+}
