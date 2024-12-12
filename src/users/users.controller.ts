a9858438e9312a08fdc33b0c72c6924c2d606e4e
Matuzay32 <eze.rey92@gmail.com>
2024-12-12 19:16:44 -0300

    removing role validation for the moment

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -17,35 +17,29 @@ import { Role } from 'src/users/enums/role.enum';
 import { RolesGuard } from 'src/auth/guards/roles.guard';
 
 @Controller('users')
-@UseGuards(JwtAuthGuard, RolesGuard)
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post()
-  @Roles(Role.ADMIN)
   create(@Body() createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto);
   }
   @Get()
-  @Roles(Role.USER)
   findAll() {
     return this.usersService.findAll();
   }
 
   @Get(':id')
-  @Roles(Role.ADMIN, Role.USER)
   findOne(@Param('id') id: string) {
     return this.usersService.findOne(+id);
   }
 
   @Patch(':id')
-  @Roles(Role.ADMIN)
   update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
     return this.usersService.update(+id, updateUserDto);
   }
 
   @Delete(':id')
-  @Roles(Role.ADMIN)
   remove(@Param('id') id: string) {
     return this.usersService.remove(+id);
   }
