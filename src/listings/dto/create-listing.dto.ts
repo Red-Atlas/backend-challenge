a6f79c9522e63432e738611f126f883a21033a6d
Matuzay32 <eze.rey92@gmail.com>
2024-12-12 13:12:41 -0300

    adding ListingDTO

--- src/listings/dto/create-listing.dto.ts
+++ src/listings/dto/create-listing.dto.ts
@@ -1,1 +1,27 @@
-export class CreateListingDto {}
+import { IsEnum, IsNumber, IsPositive, IsNotEmpty } from 'class-validator';
+import { ListingStatus } from '../enums/listing-status.enum';
+import { PropertyType } from '../enums/property-type.enum';
+
+export class CreateListingDto {
+  @IsNumber()
+  @IsPositive({ message: 'Price must be a positive number' })
+  price: number;
+
+  @IsEnum(ListingStatus, {
+    message: `Status must be one of the following: ${Object.values(ListingStatus).join(', ')}`,
+  })
+  status: ListingStatus;
+
+  @IsEnum(PropertyType, {
+    message: `Property type must be one of the following: ${Object.values(PropertyType).join(', ')}`,
+  })
+  property_type: PropertyType;
+
+  @IsNumber()
+  @IsNotEmpty({ message: 'Property ID is required' })
+  property_id: number;
+
+  @IsNumber()
+  @IsNotEmpty({ message: 'User ID is required' })
+  user_id: number;
+}
