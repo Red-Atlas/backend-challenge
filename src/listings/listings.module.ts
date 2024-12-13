8332e3a42a627309de2bb93ff348198c270d02f4
Matuzay32 <eze.rey92@gmail.com>
2024-12-13 19:39:51 -0300

    fixing import error

--- src/listings/listings.module.ts
+++ src/listings/listings.module.ts
@@ -4,11 +4,11 @@ import { ListingsController } from './listings.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Listing } from './entities/listing.entity';
 import { Property } from 'src/properties/entities/property.entity';
-import { Transaction } from 'typeorm';
 import { User } from 'src/users/entities/user.entity';
+import { Transaction } from 'src/transactions/entities/transaction.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Property, Listing, Transaction, User])],
+  imports: [TypeOrmModule.forFeature([Property, Listing, User, Transaction])],
   controllers: [ListingsController],
   providers: [ListingsService],
 })
