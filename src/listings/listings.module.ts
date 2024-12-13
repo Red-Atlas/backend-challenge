4706adcece547559c6c8258e5b16143f0d16aae8
Matuzay32 <eze.rey92@gmail.com>
2024-12-13 19:29:15 -0300

    adding entities in the listings module

--- src/listings/listings.module.ts
+++ src/listings/listings.module.ts
@@ -3,9 +3,12 @@ import { ListingsService } from './listings.service';
 import { ListingsController } from './listings.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Listing } from './entities/listing.entity';
+import { Property } from 'src/properties/entities/property.entity';
+import { Transaction } from 'typeorm';
+import { User } from 'src/users/entities/user.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Listing])],
+  imports: [TypeOrmModule.forFeature([Property, Listing, Transaction, User])],
   controllers: [ListingsController],
   providers: [ListingsService],
 })
