5fef16c320897ba10c60bffe25581865b85dcf41
Eduardo Sequeira <eduardoasm19@gmail.com>
2024-12-15 01:40:38 -0400

    port

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
 import "reflect-metadata";
+import app from './app.js';
 import { AppDataSource } from './db.js';
 
 export async function main() {
   try {
     await AppDataSource.initialize();
     console.log("database connected")
+    app.listen(process.env.PORT);
+    console.log("Server is listening on port 3000");
   } catch (error) {
     console.error(error)
   }
